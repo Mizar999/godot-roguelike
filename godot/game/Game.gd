deb79229d973e40212538f56fb8791f9b335ffd5
Mario <mizar2502@aol.com>
2020-03-11 00:32:34 +0100

    Fix pathfinder with better update connection logic

--- godot/game/Game.gd
+++ godot/game/Game.gd
@@ -17,21 +17,21 @@ func _init(map_p, debug_draw_p):
 	debug_draw = debug_draw_p
 	pathfinder = Pathfinder.new()
 	pathfinder.initialize_map(map, debug_draw)
-	# warning-ignore:return_value_discarded
-	connect(RL.SIGNAL_MAP_CHANGED, pathfinder, "update_connections")
 	
 	player = spawn("actors/player", Vector2(1, 1))
-	for cell in map.get_passable_cells(5):
+	for cell in map.get_passable_cells(15):
 		spawn("actors/goblin", cell)
 	for cell in map.get_passable_cells(25):
 		spawn("props/statue", cell).get_node("Symbol").self_modulate = Color(randf(), randf(), randf())
+	
+	pathfinder.update_connections()
 
 func spawn(path: String, cell: Vector2):
 	var entity = RL.database.create_entity(path)
 	entity.cell = cell
 	map.add_entity(entity)
 	if entity.is_in_group(RL.GROUP_BLOCKER):
-		emit_signal(RL.SIGNAL_MAP_CHANGED, [entity.cell])
+		pathfinder.dirty_cells[entity.cell] = false
 	# Cannot check for type 'Actor' with 'entity is Actor', because of cylcic reference
 	# see: https://github.com/godotengine/godot/issues/21461#issuecomment-578860188
 	if entity.has_method("take_turn"):
@@ -39,10 +39,11 @@ func spawn(path: String, cell: Vector2):
 	return entity
 
 func move(actor, cell: Vector2) -> void:
-	var old_cell = actor.cell
+	if actor.is_in_group(RL.GROUP_BLOCKER):
+		pathfinder.dirty_cells[actor.cell] = true
+		pathfinder.dirty_cells[cell] = false
 	actor.cell = cell
 	actor.position = map.map_to_world(cell)
-	emit_signal(RL.SIGNAL_MAP_CHANGED, [old_cell, cell], cell)
 
 func main_loop() -> void:
 	var actor
--- godot/map/Pathfinder.gd
+++ godot/map/Pathfinder.gd
@@ -3,12 +3,14 @@ class_name Pathfinder
 
 var astar: AStar2D
 var bounds: Rect2
+var dirty_cells
 var _map
 var _debug_draw
 
 func _init():
 	astar = AStar2D.new()
 	bounds = Rect2()
+	dirty_cells = {}
 	_map = null
 
 func initialize_map(map, debug_draw) -> void:
@@ -17,36 +19,57 @@ func initialize_map(map, debug_draw) -> void:
 	_debug_draw = debug_draw
 	_debug_draw.tilesize = _map.cell_size * _map.scale
 	bounds = map.get_used_rect()
-	var cells = map.get_floor_cells()
-	for cell in cells:
-		astar.add_point(_get_id(cell), Vector2(cell.x, cell.y))
-	update_connections(cells)
+	_initialize_connections()
 
 func get_path(from: Vector2, to: Vector2):
-	update_connections([from], to)
+	update_connections()
 	return astar.get_point_path(_get_id(from), _get_id(to))
 
-# TODO Something is wrong with this procedure
-func update_connections(cells = [], target = null) -> void:
+func update_connections() -> void:
+	var cell_id
 	var neighbor
 	var neighbor_id
-	var is_neighbor_passable
+	var is_passable
+	for cell in dirty_cells:
+		cell_id = _get_id(cell)
+		is_passable = dirty_cells[cell]
+		for x in range(-1, 2):
+			for y in range(-1, 2):
+				neighbor = Vector2(cell.x + x, cell.y + y)
+				neighbor_id = _get_id(neighbor)
+				if cell_id != neighbor_id and astar.has_point(cell_id) and astar.has_point(neighbor_id):
+					# TODO Wrong update logic: Movement from cell to neighbor must be possible,
+					# if neighbor is passable
+					if is_passable:
+						if !astar.are_points_connected(cell_id, neighbor_id):
+							astar.connect_points(cell_id, neighbor_id)
+					else:
+						if astar.are_points_connected(cell_id, neighbor_id):
+							astar.disconnect_points(cell_id, neighbor_id)
+					_debug_draw.set_connection(cell, neighbor, astar.are_points_connected(cell_id, neighbor_id))
+	dirty_cells.clear()
+	_debug_draw.update()
+
+func _get_id(cell) -> int:
+	return (cell.y * bounds.size.x) + cell.x
+
+func _initialize_connections() -> void:
+	astar.clear()
+	var cells = _map.get_floor_cells()
+	for cell in cells:
+		astar.add_point(_get_id(cell), Vector2(cell.x, cell.y))
+	
 	var cell_id
+	var neighbor
+	var neighbor_id
 	for cell in cells:
 		cell_id = _get_id(cell)
 		for x in range(-1, 2):
 			for y in range(-1, 2):
 				neighbor = Vector2(cell.x + x, cell.y + y)
 				neighbor_id = _get_id(neighbor)
-				if neighbor != cell and astar.has_point(neighbor_id):
-					is_neighbor_passable = neighbor == target or _map.is_cell_passable(neighbor)
-					if is_neighbor_passable:
-						astar.connect_points(cell_id, neighbor_id, false)
-					else:
-						if astar.are_points_connected(cell_id, neighbor_id):
-							astar.disconnect_points(cell_id, neighbor_id)
+				if cell_id != neighbor_id and astar.has_point(neighbor_id):
+					astar.connect_points(cell_id, neighbor_id)
 					_debug_draw.set_connection(cell, neighbor, astar.are_points_connected(cell_id, neighbor_id))
 	_debug_draw.update()
 
-func _get_id(cell) -> int:
-	return (cell.y * bounds.size.x) + cell.x
